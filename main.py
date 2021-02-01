69883717b87e880e59eac196e81d2b9e62994d48
unknownZandbak <barrybatszak@gmail.com>
2021-02-01 11:29:37 +0100

    OP1

---
+++ main.py
@@ -0,0 +1,50 @@
+def op1():
+    aantal = int(input("Hoe Groot? : "))
+    for i in range(0, aantal):
+        print(i*"*")
+    for x in range(aantal, 0, -1):
+        print(x*"*")
+
+
+def op2():
+    pass
+
+
+def op3():
+    pass
+
+
+def op4():
+    pass
+
+
+def op5():
+    pass
+
+
+def op6():
+    pass
+
+
+def op7():
+    pass
+
+
+def op8():
+    pass
+
+
+def op9():
+    pass
+
+
+def op10():
+    pass
+
+
+def op11():
+    pass
+
+
+if __name__ == "__main__":
+    op1()
