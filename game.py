93ce533df64c2bf5a4d9781ab81a8d8d715fd4e3
unknownZandbak <barrybatszak@gmail.com>
2021-02-08 15:45:47 +0100

    taal keuze gemaakt

--- game.py
+++ game.py
@@ -4,15 +4,16 @@ import maestro as ma
 
 # While True loop is used as a main game loop
 while True:
-
-    # Instellen van een aantal variabellen zoals de code en gok zodat ze door elke functie gewijzigd kunnen worden
-    # Voor de kleuren gebruik ik getallen van 1 t/m 6 (zes verschillende "kleuren")
-    # Dit zorgt ervoor dat er met elke nieuwe game er een schoon blad is voor de nieuwe game
+    '''
+    Instellen van een aantal variabellen zoals de code en gok zodat ze door elke functie gewijzigd kunnen worden
+    Voor de kleuren gebruik ik getallen van 1 t/m 6 (zes verschillende "kleuren")
+    Dit zorgt ervoor dat er met elke nieuwe game er een schoon blad is voor de nieuwe game
+    '''
     code = ["%ph%", "%ph%", "%ph%", "%ph%"]
-    gokken = [["%ph%", "%ph%", "%ph%", "%ph%"] for i in range(9)]
+    guesses = [["%ph%", "%ph%", "%ph%", "%ph%"] for i in range(9)]
     answer = ["%ph%", "%ph%", "%ph%", "%ph%"]
     answers = [answer for i in range(9)]
-    ronde = 0
+    round = 0
     win = False
     colorCode = {
         1: "Rood",
@@ -22,20 +23,21 @@ while True:
         5: "Zwart",
         6: "Oranje"
     }
-    # Het maken van de code door random een getal tussen 1 t/m 6 te kiezen voor ellke index
+
+    # Generate a code of 4 integers between 0 and 7
     for i in range(0, 4):
         code[i] = random.randint(1, 6)
     print(f"\nCode gegenereerd: {code}")
 
-    while ronde < 8:
+    while round < 8:
 
-        # Hier doen we nu een random gok voordat we het algoritme gaan implementeren
-        ronde += 1
-        print(f"\nGok {ronde}")
-        gokken[ronde] = ma.gok(ronde, answers, gokken)
-        print(f"Gok gegenereerd {gokken[ronde]}")
+        # make a gues
+        round += 1
+        print(f"\nGok {round}")
+        guesses[round] = ma.gok(round, answers, guesses)
+        print(f"Gok gegenereerd {guesses[round]}")
 
-        # een tijdelijke lijst voor het maken van feedback
+        # a temp code var in order to correctly create feedback
         tmp_code = [i for i in code]
 
         '''
@@ -46,28 +48,29 @@ while True:
         De positie van de getallen in het antwoord zeggen niet over welke "kleur" goed is en welke fout
         '''
         nummer = 0
-        for i in gokken[ronde]:
+        for i in guesses[round]:
             if i in tmp_code:
-                if gokken[ronde].index(i) == code.index(i):
-                    answers[ronde][nummer] = 2
+                if guesses[round].index(i) == code.index(i):
+                    answers[round][nummer] = 2
                 else:
-                    answers[ronde][nummer] = 1
+                    answers[round][nummer] = 1
                 tmp_code.remove(i)
             else:
-                answers[ronde][nummer] = 0
+                answers[round][nummer] = 0
             nummer += 1
         print(f"Feedback gegenereerd: {answer}")
 
         # Check of de code goed geraden is
-        if gokken[ronde] == code:
+        if guesses[round] == code:
             win = True
             break
     if win:
-        print(f"\n\nGefeliciteerd je hebt gewonnen in ronde {ronde} !!!!!\n")
+        print(f"\n\nGefeliciteerd je hebt gewonnen in round {round} !!!!!\n")
         break
     else:
         print(f"\n\nHelaas je hebt de code niet goed geraden\n")
         break
+
     # if input("Wil je nog een keer spelen? j of n : ") == "j":
     #     continue
     # else:
--- maestro.py
+++ maestro.py
@@ -2,11 +2,11 @@ import itertools
 '''
 Het volgende algoritme zijn gemaakt op basis van startegien die besrproken worden 
 in een paper van de universiteit van Groningen https://canvas.hu.nl/courses/22629/files/1520303/download?wrap=1. 
-In dit geval zal ik gebruik maken van de 
+In dit geval zal ik gebruik maken van de worst case strategy
 '''
 
 # allemogelijke opties genereren
-mogelijke_gokken = list(itertools.product(range(1, 7), repeat=4))
+possible_guesses = list(itertools.product(range(1, 7), repeat=4))
 
 
 def gok(ronde, answers, gokken):
@@ -20,9 +20,10 @@ def gok(ronde, answers, gokken):
 def analyse(ronde, answers, gokken):
     nummer = 0
     for i in gokken[ronde]:
-        tmp_code = [i for i in mogelijke_gokken]
+        tmp_code = [i for i in possible_guesses]
+        tmp_answer = ["%ph%", "%ph%", "%ph%", "%ph%"]
         if i in tmp_code:
-            if gokken[ronde].index(i) == mogelijke_gokken.index(i):
+            if gokken[ronde].index(i) == possible_guesses.index(i):
                 answers[ronde][nummer] = 2
             else:
                 answers[ronde][nummer] = 1
